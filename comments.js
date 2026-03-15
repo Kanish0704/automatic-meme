35ccc9951e8286933a81fdbebdf057389455a801
KANISHKA R B <126150021@sastra.ac.in>
2026-03-15 14:41:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,9 @@
-// create web server
+// create a simple web server
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -11,5 +12,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-}); 
-
+});
